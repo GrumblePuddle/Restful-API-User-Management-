d66601e39f2a0ec6859f5087b234a3bae50f48f4
Gustav Liffner <gurra_liffner@hotmail.com>
2025-03-28 00:53:22 +0000

    Swagger imported

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.json());
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "User API",
+      version: "1.0.0",
+      description: "A simple Express User API",
+    },
+  },
+  apis: ["./server.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
 let users = [
@@ -17,9 +34,76 @@ let users = [
   { id: 4, name: "Ines Liffner", age: "16", hobby: "Fotboll" },
 ];
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of all users
+ *     description: Retrieve a list of users in the library
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   age:
+ *                     type: string
+ *                   hobby:
+ *                     type: string
+ */
+
 app.get("/users", (req, res) => {
   res.json(users);
 });
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Add a new user
+ *     description: Add a new user to the library
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - age
+ *               - hobby
+ *             properties:
+ *               name:
+ *                 type: string
+ *               age:
+ *                 type: string
+ *               hobby:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 age:
+ *                   type: string
+ *                 hobby:
+ *                   type: string
+ */
 
 app.post("/users", (req, res) => {
   const newUser = {
@@ -31,6 +115,56 @@ app.post("/users", (req, res) => {
   users.push(newUser);
   res.json({ message: "New user created", user: newUser });
 });
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update existing user
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric ID of the user to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               age:
+ *                 type: string
+ *               hobby:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 age:
+ *                   type: string
+ *                 hobby:
+ *                   type: string
+ *       404:
+ *        description: User not found
+ */
 
 app.put("/users/:id", (req, res) => {
   const userId = parseInt(req.params.id);
@@ -44,6 +178,32 @@ app.put("/users/:id", (req, res) => {
     res.json({ message: `User with id ${userId} updated`, user });
   }
 });
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric ID of the user to delete
+ *   responses:
+ *     200:
+ *        description: User deleted successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *     404:
+ *      description: User not found
+ */
 
 app.delete("/users/:id", (req, res) => {
   const userId = parseInt(req.params.id);
